2b56716657648fd709752d5bee11e147dbea7a32
Zhang Chen <329188614@qq.com>
2023-03-31 23:47:41 +0800

    inital ui

---
+++ qBittorrentAssistant/MainViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.ComponentModel;
+using QBittorrent.Client;
+
+namespace qBittorrentAssistant
+{
+    internal partial class MainViewModel: ObservableObject
+    {
+        public MainViewModel()
+        {
+            //var client = new QBittorrentClient(new Uri("http://localhost:8080/"));
+            ////await client.LoginAsync("login", "password");
+            //var result = client.GetTorrentListAsync();
+            //var r = result.Result;
+        }
+    }
+}
