59ae04228beff6aee41c577d798706a2a1f2ba1a
Zhang Chen <329188614@qq.com>
2023-06-03 20:43:50 +0800

    add image for torrent

---
+++ qBittorrentAssistant/Resource/DataTemplate/TorrentIconDataTemplateSelector.cs
@@ -0,0 +1,32 @@
+﻿using qBittorrentAssistant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace qBittorrentAssistant.Resource.DataTemplate
+{
+    public class TorrentIconDataTemplateSelector : DataTemplateSelector
+    {
+        //public System.Windows.DataTemplate IconTemplate { get; set; }
+
+        //public override System.Windows.DataTemplate SelectTemplate(object item, DependencyObject container)
+        //{
+        //    if (item is DirectoryTreeItem o)
+        //    {
+        //        if (o.IsContainByTorrent)
+        //        {
+        //            return IconTemplate;
+        //        }
+        //        else
+        //        {
+        //            return null;
+        //        }
+        //    }
+        //    return null;
+        //}
+    }
+}
